5322992eda0d80ad189cad138e18e764ff8521f7
AndrewDanial <adanial9379@gmail.com>
2022-11-28 18:50:00 -0500

    Splashscreen!

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,29 @@
+import { ButtonGroup } from "@mui/material";
+import Button from "@mui/material/Button";
 export default function SplashScreen() {
+
+    const buttonStyle = {
+        marginLeft: "10%",
+        marginRight: "10%",
+        width: "500px",
+        height: "100px"
+    }
+
     return (
         <div id="splash-screen">
-            Playlister
-        </div>
+            <div>
+                <img src="playlisterlogo.png" alt="Playlister Logo"></img>
+                <div id="subtitle">
+                    Create, edit, and share playlists here!
+                </div>
+            </div>
+
+            <ButtonGroup id="splashbuttons" sx={{}}>
+                <Button variant="contained" sx={buttonStyle}>Register</Button>
+                <Button variant="contained" sx={buttonStyle}>Login</Button>
+                <Button variant="contained" sx={buttonStyle}>Continue As Guest</Button>
+            </ButtonGroup>
+        </div >
+
     )
 }
